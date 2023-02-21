f4ac5476d87e23d9893b7e213ef31dbe4089b20b
DenisProcyon <denis.shadrin@procyon.ee>
2023-02-21 18:55:09 +0200

    update new

--- src/Sheep.java
+++ src/Sheep.java
@@ -3,38 +3,33 @@ import java.util.Arrays;
 public class Sheep {
 
    enum Animal {sheep, goat};
-   
+
    public static void main (String[] param) {
+      // for debugging
 
       Animal[] animals = {Animal.sheep, Animal.sheep, Animal.sheep, Animal.sheep, Animal.goat};
 
       reorder(animals);
    }
-   
+
    public static void reorder (Animal[] animals) {
-      Integer sheep = 0;
-      Integer goats = 0;
+      // TODO!!! Your program here
+      Integer animal = 0;
 
       for (int i =0; i < animals.length; i++){
-         if (animals[i] == Animal.sheep){
-            sheep++;
-         }
-         else {
-            goats++;
+         if (animals[i] == Animal.goat){
+            animal++;
          }
       }
 
       int animal_index = 0;
-      for (int i = 0; i < goats; i++){
+      for (int i = 0; i < animal; i++){
          animals[animal_index] = Animal.goat;
          animal_index++;
       }
-      for (int i = 0; i < sheep; i++){
+      for (int i = 0; i < animals.length-animal; i++){
          animals[animal_index] = Animal.sheep;
          animal_index++;
       }
-
-      //System.out.println(Arrays.toString(animals));
    }
 }
-
